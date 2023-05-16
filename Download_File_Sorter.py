24fdbfef497351c6867a01c33ba9d5f540581be1
gsikora1028 <gabrielsikora28@gmail.com>
2023-05-16 15:01:47 -0700

    general edits

--- Download_File_Sorter.py
+++ Download_File_Sorter.py
@@ -6,19 +6,23 @@ import time
 
 # Define the folders for different file types
 folders = {
-    # Images
-    ".JPG": "Images", ".png": "Images", ".jpg": "Images", ".JPEG": "Images", 
-    ".jpeg": "Images", ".HEIC": "Images",".heic": "Images", ".svg": "Images",
-    
-    # Documents
-    ".xlsx": "Documents", ".xls": "Documents", ".pptx": "Documents", 
-    ".docx": "Documents", ".pdf": "Documents",
-    
-    # Videos
-    ".mp4": "Videos",
-    
-    # MISC
-    ".dmg": "MISC", ".btw": "MISC", ".zip": "MISC"
+    #Audio
+    ".adt" : "Audio", ".adts" : "Audio", ".aac" : "Audio", ".m4a" : "Audio", ".aifc" : "Audio", ".aiff" : "Audio", ".aif" : "Audio", ".avi" : "Audio", ".cda" : "Audio", ".m4a" : "Audio", ".wav" : "Audio",
+    #Documents
+    ".csv" : "Documents", ".dif" : "Documents", ".doc" : "Documents", ".docm" : "Documents", ".docx" : "Documents", ".dot" : "Documents", ".dotx" : "Documents", ".pdf" : "Documents", ".pot" : "Documents", ".potm" : "Documents", 
+    ".potx" : "Documents", ".ppam" : "Documents", ".pps" : "Documents", ".ppsm" : "Documents", ".ppsx" : "Documents", ".ppt" : "Documents", ".pptm" : "Documents", ".pptx" : "Documents", ".wp5" : "Documents", ".wpd" : "Documents", 
+    ".xla" : "Documents", ".xlam" : "Documents", ".xll" : "Documents", ".xlm" : "Documents", ".xls" : "Documents", ".xlsm" : "Documents", ".xlsx" : "Documents", ".xlt" : "Documents", ".xltm" : "Documents", ".xltx" : "Documents", 
+    ".xps" : "Documents", ".sldm" : "Documents", ".sldx" : "Documents", ".tmp" : "Documents", ".txt" : "Documents",
+    #Email
+    ".eml" : "Email", ".pst" : "Email",
+    #Images
+    ".gif" : "Images", ".iso" : "Images", ".jpeg" : "Images", ".jpg" : "Images", ".psd" : "Images", ".png" : "Images", ".psd" : "Images", ".tiff" : "Images", ".tif" : "Images",
+    #MISC
+    ".vsd" : "MISC", ".vsdm" : "MISC", ".vsdx" : "MISC", ".vss" : "MISC", ".vssm" : "MISC", ".vst" : "MISC", ".vstm" : "MISC", ".vstx" : "MISC", ".zip" : "MISC", ".step" : "MISC", ".bin" : "MISC", ".bmp" : "MISC", ".exe" : "MISC", 
+    #Videos
+    ".flv" : "Videos", ".mov" : "Videos", ".mp3" : "Videos", ".mp4" : "Videos", ".mp4" : "Videos", ".mpeg" : "Videos", ".mpg" : "Videos", ".vob" : "Video",
+    #Webpages
+    ".aspx" : "Webpages", ".html" : "Webpages",".htm" : "Webpages",
 }
 
 # Loop through the files in the downloads folder
@@ -28,7 +32,7 @@ for filename in os.listdir("/Users/gsikora/Downloads"):
         folder = "TRF_Files"  # Specify the folder for TRF files
     else:
         # Get the file extension and corresponding folder
-        ext = os.path.splitext(filename)[1]
+        ext = os.path.splitext(filename)[1].lower()
         folder = folders.get(ext)
 
     # If the file extension is in the dictionary, move the file to the corresponding folder
@@ -38,7 +42,6 @@ for filename in os.listdir("/Users/gsikora/Downloads"):
         source_path = Path("/Users/gsikora/Downloads") / filename
         destination_path = destination_folder / filename
         shutil.move(str(source_path), str(destination_path))
-
 
 # define the list of directories where you want to search for files with .xls extension
 directories = ['/Users/gsikora/Downloads/Documents', '/Users/gsikora/Desktop']
